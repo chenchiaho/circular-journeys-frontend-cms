7b63483e0b85cd54e3dcb9f0ca048fabda0ddc17
Alan Chou <alanchou19950414@gmail.com>
2022-12-21 12:04:34 +0800

    新增登入首頁 login-page

--- content-management-system/login.php
+++ content-management-system/login.php
@@ -4,35 +4,29 @@ $pageName = 'login';
 
 ?>
 <?php include './parts/html-head.php' ?>
-<?php include './parts/navbar.php' ?>
 
-<div class="container">
-    <div class="row">
-        <div class="col-lg-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">管理者登入</h5>
-                    <form name="form1" onsubmit="checkForm(event)">
-                        <div class="mb-3">
-                            <label for="account_id" class="form-label">帳號</label>
-                            <input type="text" class="form-control" id="account_id" name="account_id" required>
-                            <div class="form-text"></div>
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="password" class="form-label">密碼</label>
-                            <input type="password" class="form-control" id="password" name="password">
-                            <div class="form-text"></div>
-                        </div>
-                        <button type="submit" class="btn btn-primary">登入</button>
-                    </form>
-
+<body>
+    <div class="container card text-white bg-dark justify-content-center" style="padding: 10px;width:30%;">
+        <div class="card-body">
+            <h5 class="card-title">管理者登入</h5>
+            <form name="form1" onsubmit="checkForm(event)">
+                <div class="mb-3">
+                    <label for="account_id" class="form-label">帳號</label>
+                    <input type="text" class="form-control" id="account_id" name="account_id" required>
+                    <div class="form-text"></div>
                 </div>
-            </div>
+
+                <div class="mb-3">
+                    <label for="password" class="form-label">密碼</label>
+                    <input type="password" class="form-control" id="password" name="password">
+                    <div class="form-text"></div>
+                </div>
+                <button type="submit" class="btn btn-outline-success btn-lg">登入</button>
+            </form>
         </div>
     </div>
+</body>
 
-</div>
 <?php include './parts/scripts.php' ?>
 <script>
     function checkForm(e) {
@@ -42,15 +36,15 @@ $pageName = 'login';
         const fd = new FormData(document.form1);
 
         fetch('login-api.php', {
-                method: 'POST',
-                body: fd
-            })
+            method: 'POST',
+            body: fd
+        })
             .then(r => r.json())
             .then(data => {
                 console.log(data);
                 if (data.success) {
                     // alert('成功登入');
-                    location.href = './marketplace-page/list-admin.php';
+                    location.href = 'home.php';
                 } else {
                     alert(data.error);
                 }
