dd83d7b7fd1881a3af7694589766e3dffad6e1d0
Circle <ja95175@gmail.com>
2022-12-24 17:13:29 +0800

    edit-page

--- content-management-system/blog-page/blog-edit.php
+++ content-management-system/blog-page/blog-edit.php
@@ -1,7 +1,23 @@
 <?php
 require '../parts/connect_db.php';
 
+$post_id = isset($_GET['post_id']) ? intval($_GET['post_id']) : 0;
+
+if(empty($post_id)){
+    header('Location: blog-admin.php');
+    exit;
+}
+
+$sql = "SELECT * FROM `post` WHERE post_id=$post_id";
+
+$r = $pdo->query($sql)->fetch();
+if(empty($r)){
+    header('Location: blog-admin.php');
+    exit;
+}
+
 ?>
+
 <?php include '../parts/html-head.php' ?>
 <?php include '../parts/navbar.php' ?>
 <br>
@@ -15,30 +31,29 @@ require '../parts/connect_db.php';
     <div class="row">
         <div class="card text-center p-0">
           <div class="card-header">
-            新增文章
+            修改文章編號：<?= $r['post_id'] ?>
           </div>
 
           <div class="card-body">
-            <!-- <form name="post_form" method="post" action="insert-api.php"> -->
             <form name="post_form" onsubmit="checkForm(event)" novalidate>
+                <input type="hidden" name="post_id" value="<?= $r['post_id'] ?>">
                 <div class="mb-3">
                     <label for="post_title" class="h3">文章標題</label>
                     <br>
-                    <input type="text" id="post_title" name="post_title"  class="form-control" required>
+                    <input type="text" id="post_title" name="post_title"  class="form-control" value="<?= htmlentities($r['post_title']) ?>">
                     <div class="form-text"></div>
                 </div>
     
                 <div class="mb-3">
                     <label for="post_content" class="card-text">文章內文</label>
                     <br>
-                    <textarea name="post_content" id="post_content" class="form-control" cols="30" rows="10" required></textarea>
+                    <textarea name="post_content" id="post_content" class="form-control" cols="30" rows="10"><?= htmlentities($r['post_content']) ?></textarea>
                     <div class="form-text"></div>
                 </div>
 
                 <br>
-                <button type="submit" class="btn btn-secondary">送出</button>
+                <button type="submit" class="btn btn-secondary">修改</button>
             </form>
-
           </div>
         </div>
     </div>
@@ -72,23 +87,22 @@ require '../parts/connect_db.php';
 
         const fd = new FormData(document.post_form);
         
-        fetch('insert-api.php',{
+        fetch('edit-api.php',{
             method: 'POST',
             body: fd
         })
-        .then(r=> r.json())
+        .then(r=> r.text())
         .then(obj=>{
             console.log(obj);
             if(obj.success){
-                alert('新增成功');
-                location.href = 'blog-insert.php';
+                alert('修改成功');
             }else{
                 for(let k in obj.errors){
                     const el = document.querySelector('#'+k);
                     // console.log(el);
                     if(el){
                         el.style.border = '2px solid red';
-                        $(el).next().innerHTML = obj.errors[k];
+                        el.nextElementSibling.innerHTML = obj.errors[k];
                     }
                 }
             }
--- content-management-system/blog-page/edit-api.php
+++ content-management-system/blog-page/edit-api.php
@@ -8,6 +8,13 @@ $ouput =[
     'code' => 0,
     'errors' => []
 ];
+
+if(empty($_POST['post_id'])){
+    $ouput['errors']['post_id'] = '沒有文章編號';
+    echo json_encode($ouput, JSON_UNESCAPED_UNICODE);
+    exit;
+}
+
 if(empty($_POST['post_title']) && empty($_POST['post_content'])){
     $ouput['errors']['post_title'] = '沒有文章標題';
     $ouput['errors']['post_content'] = '沒有文章內文';
@@ -24,6 +31,8 @@ if(empty($_POST['post_title']) && empty($_POST['post_content'])){
 }
 
 $isPass = true;
+$post_id =intval($_POST['post_id']);
+
 
 if(mb_strlen($_POST['post_title']) < 2){
     $ouput['errors']['post_title'] = '請正確輸入文章標題';
@@ -36,14 +45,18 @@ if(mb_strlen($_POST['post_content']) < 2){
 }
 
 
-$sql = "INSERT INTO `post`(`created_date`, `modified_date`, `post_title`, `post_content`) 
-        VALUES (NOW(),null,?,?)";
+$sql = "UPDATE `post` SET 
+        `modified_date`=NOW(),
+        `post_title`=?,
+        `post_content`=?, WHERE `post_id`=?";
+
 $stmt = $pdo -> prepare($sql);
 
 if($isPass){
     $stmt -> execute([
         $_POST['post_title'],
         $_POST['post_content'],
+        $post_id
     ]);
     $ouput['success'] = !! $stmt->rowCount();
 }
